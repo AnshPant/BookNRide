ec2e9037644d46ec0f0482c5c9c4f29f82e9c95d
AnshPant <72350344+AnshPant@users.noreply.github.com>
2022-07-31 20:18:53 +0530

    Update RideSelector.js

--- pages/components/RideSelector.js
+++ pages/components/RideSelector.js
@@ -1,8 +1,31 @@
 import  React, {useEffect,useState} from 'react'
 import tw from "tailwind-styled-components"
-import { carList } from '../data/carList'
-import Link from 'next/link'
 
+
+
+const carList = [
+    {
+      imgUrl: 'https://i.ibb.co/cyvcpfF/uberx.png',
+      service: 'White Car- Medium',
+      multiplier: 1,
+    },
+    {
+      imgUrl: 'https://i.ibb.co/YDYMKny/uberxl.png',
+      service: 'White Car Large',
+      multiplier: 1.5,
+    },
+    
+    {
+      imgUrl: 'https://i.ibb.co/cyvcpfF/uberx.png',
+      service: 'Economical Car - Large',
+      multiplier: 1.2,
+    },
+    {
+      imgUrl: ' https://i.ibb.co/1nStPWT/uberblacksuv.png',
+      service: 'Black Car- Large',
+      multiplier: 2.8,
+    }
+  ]
 
 const RideSelector = (props) => {
     const [rideDuration,setRideDuration] = useState(0)
